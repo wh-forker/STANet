a91c889913c59866ae92024eb89e8f02263284d0
GuotaoWang <42886966+guotaowang@users.noreply.github.com>
2021-05-20 15:44:12 +0800

    Update generateframe.py

--- generateframe.py
+++ generateframe.py
@@ -1,15 +1,15 @@
 import cv2 as cv
 import os
 
-path_video = .\Data\AVE\' 
-path_save = '.\Data\Img3\' 
+path_video = ./Data/AVE/' 
+path_save = './Data/Img3/' 
 
 for root, dirs, files_video in os.walk(path_video):
     for files_name in files_video:
         print(files_name)
         count = 0
         video_address = path_video + files_name
-        os.makedirs(path_save+files_name[:-4] + '\\', exist_ok=True)
+        os.makedirs(path_save+files_name[:-4] + '/', exist_ok=True)
         video = cv.VideoCapture(video_address)
         if (video.isOpened() == False):
             print("error opening video stream or file!")
@@ -19,7 +19,7 @@ for root, dirs, files_video in os.walk(path_video):
             if ret == False:
                 break
             # time_stamp_fromvideo = video.get(cv.CAP_PROP_POS_MSEC)
-            pic_path = path_save+files_name[:-4] + '\\'
+            pic_path = path_save+files_name[:-4] + '/'
             frame = cv.resize(frame, (400,400))
             cv.imwrite(pic_path + str(count).zfill(4) + '.jpg', frame)
             # print(time_stamp_fromvideo)
